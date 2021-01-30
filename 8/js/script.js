5a265afd4f4db3d9571b08c99778cb8bf5feec1c
Shuya Sawa <sawa.shuya.infratop@gmail.com>
2021-01-30 07:24:28 +0000

    finish 8

---
+++ 8/js/script.js
@@ -0,0 +1,21 @@
+$(".theTarget").skippr({
+  transition : "fade",
+
+  speed : 1000,
+
+  easing : "easeOutQuart",
+
+  navType : "block",
+
+  childrenElementType : "div",
+
+  arrows : true,
+
+  autoPlay : true,
+
+  autoPlayDuration : 5000,
+
+  keybordOnAlways : true,
+
+  hidePrevious : false
+});
